9da8636111aa43d54ab29be6d44a149d16d607fc
yusufmim <yusufmim2001@gmail.com>
2025-05-29 20:31:43 +0300

    init commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+('Mr. Chocolate', 8, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 6, 'F', 'Black', 'Friendly', 1),
+('Sergeant Brown', 9, 'M', 'Brown', 'Serious', 1),
+('Melissa', 4, 'F', 'White', 'Playful', 1),
+('Grinch', 7, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 3, 'F', 'Golden', 'Cheerful', 1),
+(NULL, 10, 'M', 'Gray', 'Lazy', 0);
